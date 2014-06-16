16287a6fde57b5c83f964b9eb89a88d4046f2bb9
Lew Ayotte <lew@lewayotte.com>
2014-06-16 07:32:15 -0400

    Adding rewrite rule flush check

--- pigeonpack.php
+++ pigeonpack.php
@@ -18,7 +18,7 @@ Special Thanks:
 Yusuke Kamiyamane - http://p.yusukekamiyamane.com/ - http://www.iconfinder.com/search/?q=iconset%3Afugue
 Turbomilk - http://graphicriver.net/free/web-icon-set/ - http://www.turbomilk.com/ - http://www.iconfinder.com/search/?q=iconset%3Awebset
 Minifiers:
-http://www.jsmini.com/
+http://www.minifyjs.com/javascript-compressor/
 http://www.minifycss.com/css-compressor/
 */
 
@@ -61,3 +61,34 @@ function pigeonpack_plugins_loaded() {
 
 }
 add_action( 'plugins_loaded', 'pigeonpack_plugins_loaded', 4815162342 ); //wait for the plugins to be loaded before init
+
+/**
+ * WordPress Activation Hook: does things that we need to keep track of during activation
+ *
+ * @since 1.0.1
+ */
+function pigeonpack_plugin_activation() {
+
+	update_option( 'pigeonpack_flush_rewrite_rules', 'true' );
+	
+}
+register_activation_hook( __FILE__, 'pigeonpack_plugin_activation' );
+
+/**
+ * Check to see if we need to flush the WordPress rewrite rules
+ *
+ * @since 1.0.1
+ */
+function pigeonpack_flush_rewrite_rules() {
+
+	if ( 'true' === get_option( 'pigeonpack_flush_rewrite_rules' ) ) {
+
+        // ATTENTION: This is *only* done during plugin activation hook in this example!
+        // You should *NEVER EVER* do this on every page load!!
+        flush_rewrite_rules();
+        delete_option( 'pigeonpack_flush_rewrite_rules' );
+
+    }
+    
+}
+add_action( 'init', 'pigeonpack_flush_rewrite_rules' );
